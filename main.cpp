a16b16461b92747d295b0e5e4dd0782e1ac9bccb
Sourav <205398826+morningstarxcdcode@users.noreply.github.com>
2025-05-07 06:12:10 +0530

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "include/defender.hpp"
-
-int main() {
-    std::cout << "🛡️  WinRT App Behavior Monitor Started" << std::endl;
-    monitorApps();
-    blockThreats();
-    return 0;
-}
